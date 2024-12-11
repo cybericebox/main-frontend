21217241dec619ad69005bed762c9ed5ab120596
Volodymyr Porokhniak <volodymyr.porokhniak@cybericebox.com>
2024-12-11 23:40:07 +0200

    update auth redirect

--- src/api/baseAPI.ts
+++ src/api/baseAPI.ts
@@ -12,9 +12,9 @@ export const baseAPI = axios.create({
 // Add a response interceptor to handle errors and refresh page
 baseAPI.interceptors.response.use((response: AxiosResponse) => response,
     (error) => {
-        if (error.response?.status === 401) {
+        if (error.response?.status === 401 || error.response?.status === 403) {
             if (typeof window !== "undefined") {
-                window.onbeforeunload = () => false
+                window.onbeforeunload = null
                 window.location.reload();
             }
         }
