c2292cb4f9c16330c8bbd1b1ec2cee50b8aa9115
Volodymyr Porokhniak <porokhniakvolodymyr@gmail.com>
2025-10-30 13:49:32 +0200

    Update footer copyright year to 2025

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
-import type React from "react"
+import type React from 'react'
 
 export default function Footer() {
     return (
         <footer className="py-1 bg-primary text-white rounded-t">
             <div className="container text-xs">
-                <center>© 2024 ХНУРЕ<br/><a href="https://ice.nure.ua/ua/">За підтримки кафедри ІКІ ім. В. В.
+	            <center>© 2025 ХНУРЕ<br /><a href='https://ice.nure.ua/ua/'>За підтримки кафедри ІКІ ім. В. В.
                     Поповського</a><br/>Харківського національного університету радіоелектроніки
                 </center>
             </div>
